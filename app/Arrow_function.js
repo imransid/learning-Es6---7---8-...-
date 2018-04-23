1aad4341f75343125ff6ae33942a58f8642ad57d
imransid <emailofimran1992@gmail.com>
2018-04-24 03:29:55 +0600

    Black Wolf Arrow Function

--- app/Arrow_function.js
+++ app/Arrow_function.js
@@ -5,7 +5,7 @@
 
 this.a = 60;
 
-function print(){
+function print(){ //all object acces inside function
 	this.a = 50;
 	console.log("this.a = "+this.a);
 }
@@ -24,9 +24,13 @@ let Arraypoint = () => {
 Arraypoint();
 
 
+
+
+
+
 let eye = "eye";
 
 const fire = () => {
-   return `bulls-+${eye}`;
- };
+   return `bulls-${eye}`;
+};
 
