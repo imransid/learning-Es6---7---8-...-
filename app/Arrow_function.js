dd3656f4d8ae400f6dd18926236a433389f82963
imransid <emailofimran1992@gmail.com>
2018-04-24 03:26:59 +0600

    Black Wolf Arrow Function

--- app/Arrow_function.js
+++ app/Arrow_function.js
@@ -1,1 +1,32 @@
 /*jshint esversion: 6 */
+
+
+// why we use arrow function coz it will never bind a object exm:
+
+this.a = 60;
+
+function print(){
+	this.a = 50;
+	console.log("this.a = "+this.a);
+}
+
+print();
+
+
+// Now i show ES6
+
+
+
+let Arraypoint = () => {
+	console.log("this.a = "+this.a);
+};
+
+Arraypoint();
+
+
+let eye = "eye";
+
+const fire = () => {
+   return `bulls-+${eye}`;
+ };
+
